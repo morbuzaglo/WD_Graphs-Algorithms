0c0527b9f8c968211b270d4af3e8d7f50742af21
shadyganaem <74612134+shadyganaem@users.noreply.github.com>
2021-01-13 00:14:30 +0200

    load and save

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,5 +1,8 @@
 import copy
 from typing import List
+import json
+
+import self as self
 
 from src.DiGraph import DiGraph
 from src.GraphAlgoInterface import GraphAlgoInterface
@@ -12,10 +15,52 @@ class GraphAlgo(GraphAlgoInterface):
         self.dists = {}
 
     def load_from_json(self, file_name: str) -> bool:
-        pass
+        new_graph=DiGraph()
+        f = open(file_name, "r")
+        universe_json=f.read()
+        f.close()
+        universe_dict=json.loads(universe_json)
+        all_edges=universe_dict.get("Edges")
+        all_nodes=universe_dict.get("Nodes")
+        for node in all_nodes:
+            if node.get("pos") is not None:
+                new_graph.add_node(node.get("id"),tuple(map(float,node.get("pos").split(','))))
+            else:
+                new_graph.add_node(all_nodes[node].get("id"))
+
+        for edge in all_edges:
+            new_graph.add_edge(int(edge.get("src")),int(edge.get("dest")),float(edge.get("w")))
+        self.graph=new_graph
+        return True
+    #TODO how to do try and except
+
+
+
 
     def save_to_json(self, file_name: str) -> bool:
-        pass
+        f = open(file_name, "w")
+        edges_list = []
+        nodes_list = []
+        for keys in self.graph.get_all_e().keys():
+            for keys_out in self.graph.all_out_edges_of_node(keys).keys():
+                temp_edge_dict={"src":keys,"w":self.graph.all_out_edges_of_node(keys).get(keys_out),"dest":keys_out}
+                edges_list.append(temp_edge_dict)
+            pos_tuple = self.graph.get_all_v().get(keys)
+            pos_str = ",".join(map(str,pos_tuple))
+            temp_node_dict = {"pos": pos_str, "id": keys}
+            nodes_list.append(temp_node_dict)
+        universe={"Edges":edges_list,"Nodes": nodes_list}
+        universe_json=json.dumps(universe)
+        f.write(universe_json)
+        f.close()
+        return True
+    #TODO how to do try and except
+
+
+
+
+
+
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         if id1 is id2:
--- src/shady.py
+++ src/shady.py
@@ -1,42 +1,21 @@
-from NodeData import NodeData
 import copy
+from src.GraphAlgo import GraphAlgo
+
 
 from src.DiGraph import DiGraph
 
 
-def check_node():
-    node1 = NodeData(5, 6, 'shady')
-    x=node1.getKey()
-    print(node1.getKey())
-    print(node1.getWeight())
-    node2 = NodeData(5, 6, 'shady')
-    node3 = NodeData(5, 6, 'shady')
-    node4 = NodeData(5, 6, 'shady')
-    node1.addNei(node2)
-    node1.addNei(node3)
-    node1.addNei(node3)
-    print(node2.getKey())
-    print(node1.getNeis())
-    print(node1.hasNei(node4.getKey()))
-    print(node1.getNeis())
 
-
-def check_geolocation():
-    node1 = NodeData(5, 6, 'shady',(0,1,2))
-    node2=NodeData(5, 6, 'shady')
-    print(node1.getLoc())
-    print(node2.getLoc())
-    node2.setLoc((2,7.5,9))
-    print(node2.getLoc())
 
 
 def check_graph():
     g = DiGraph()
+    algo=GraphAlgo(g)
 
-    g.add_node(0, (2.0, 3.0))
-    g.add_node(1, (1.0, 7.0))
-    g.add_node(2, (2.0, 6.0))
-    g.add_node(3, (6.0, 6.0))
+    g.add_node(0, (2.0, 3.0,9))
+    g.add_node(1, (1.0, 7.0,9))
+    g.add_node(2, (2.0, 6.0,9))
+    g.add_node(3, (6.0, 6.0,9))
 
     g.add_edge(0, 1, 6.0)
     g.add_edge(0, 2, 6.0)
@@ -45,12 +24,16 @@ def check_graph():
 
     print(g.get_all_e())
 
-    print("0 is removed? = ", g.remove_node(0))
+    #print("0 is removed? = ", g.remove_node(0))
 
     print(g.get_all_e())
     print(g.get_all_v())
 
     print("mc = ", g.get_mc())
+    algo.save_to_json("this.JSON")
+    algo.load_from_json("this.JSON")
+    algo.save_to_json("this1.JSON")
+
 
 
 
